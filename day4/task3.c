23440d989c0e853ce73b5b0f9c60ba4d101bf3b2
mac <mac@macpro.local>
2026-01-02 13:17:33 +0530

    feat: add day4 task3 and task4

---
+++ day4/task3.c
@@ -0,0 +1,16 @@
+// 02 jan 2026
+
+// Print numbers from 1 to 15 using while loop
+
+#include<stdio.h>
+
+int main()
+{
+    int i = 1;
+    while( i < 16 )
+    {
+        printf("\n%d",i);
+        i++;
+    }
+    return 0;
+}
---
+++ day4/task4.c
@@ -0,0 +1,17 @@
+// 02 jan 2026
+
+// Print numbers from 1 to 10 using do-while loop
+
+#include<stdio.h>
+ 
+int main()
+ {
+
+    int i = 1;
+    do
+    {
+        printf("\n%d",i);
+        i++;
+    } while ( i < 11 );
+    return 0;
+ }
