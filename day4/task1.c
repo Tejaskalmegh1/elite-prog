f526ccd0f032b8a40dcf8782510d1ee62ac0aa5b
mac <mac@macpro.local>
2026-01-02 10:12:44 +0530

    feat: add day4 task1

---
+++ day4/task1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int VarOne = 10;
+    float VarTwo = 77.77; 
+    char VarThree = 'A';
+    printf("\n Variable One: %d",VarOne);
+    printf("\n Variable Two: %f",VarTwo);
+    printf("\n Variable Three: %c",VarThree);
+    return 0;
+}
