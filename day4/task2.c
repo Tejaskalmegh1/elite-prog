4942e7f6d8b63c948a09ef86abc8ce85b94b4d71
mac <mac@macpro.local>
2026-01-02 13:05:02 +0530

    feat: add day4 task2

---
+++ day4/task2.c
@@ -0,0 +1,12 @@
+// 02 jan 2026
+
+// Write a C program that: Declares a char variable grade. Stores a single character (e.g., 'A') print the character
+
+#include<stdio.h>
+
+int main()
+{
+    char grade = 'A';
+    printf("\nMy Grade: %c",grade);
+    return 0;
+}
