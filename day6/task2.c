ddca8f9ffca610832f3fc9fae9e6dc3b807a2fad
tejaskalmegh1 <er.tejaskalmegh@gmail.com>
2026-01-07 11:18:38 +0530

    feat(day6): add task2

---
+++ day6/task2.c
@@ -0,0 +1,18 @@
+// 07 jan 2026
+
+// Task 21: Multiple Assignments
+// Objective: Chain assignment
+// Assign the same value to three variables using a single statement.   i/p: 5   o/p: a=5, b=5, c=5
+
+#include<stdio.h>
+
+int main()
+{
+    int a , b , c ; 
+    
+    a =  b =  c = 5 ;
+    
+    printf("\n A : %d \n B : %d \n C : %d",a,b,c);
+    
+    return 0;
+}
