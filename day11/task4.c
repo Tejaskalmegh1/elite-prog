23219ed3b15484d1623f4d15911394391a9c111d
tejaskalmegh1 <tejaskalmegh123@gmail.com>
2026-01-16 17:16:53 +0530

    update: update task4

--- day11/task4.c
+++ day11/task4.c
@@ -9,11 +9,13 @@ int main()
     int year;
 
     printf("\n Enter a year: ");
-    scanf("%d", &year);
+    scanf("%d",&year);
 
     if ((year % 400 == 0) || year % 100 != 0 && year % 4 == 0 ) {
         printf("\n %d is a Leap Year",year);
-    } else {
+    } 
+    
+    else {
         printf("\n %d is NOT a Leap Year",year);
     }
 
